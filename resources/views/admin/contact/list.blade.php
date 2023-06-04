6811993023671f123d945beda3d3be630d4c56ee
KIETKUN\dotua <dotuankiet00@gmail.com>
2023-06-04 19:23:23 +0700

    update

--- resources/views/admin/contact/list.blade.php
+++ resources/views/admin/contact/list.blade.php
@@ -31,13 +31,13 @@
           <div class="recent_heading">
             <h4>Hỗ trợ</h4>
           </div>
-          <div class="srch_bar">
+          {{-- <div class="srch_bar">
             <div class="stylish-input-group">
               <input type="text" class="search-bar" placeholder="Search">
               <span class="input-group-addon">
                 <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
               </span> </div>
-          </div>
+          </div> --}}
         </div>
         <div class="inbox_chat" id="myDIV">
           @foreach ($customers as $customer)
