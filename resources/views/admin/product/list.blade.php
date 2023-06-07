61e99c0f70bb664477946fbec5835fc609f83497
KIETKUN\dotua <dotuankiet00@gmail.com>
2023-06-07 20:02:54 +0700

    update

--- resources/views/admin/product/list.blade.php
+++ resources/views/admin/product/list.blade.php
@@ -10,11 +10,11 @@
       <i class="fas fa-search"></i>
     </button>
   </form>
-  <form role="search" method="get" action="/admin/products/list" class="row col-lg-9">
+  <form role="search" method="get" action="/admin/products/list" class="row col-lg-7">
     <div class="form-group mb-0 d-flex align-items-center justify-content-around col-lg-5">
       <label class="mb-0">Loại sản phẩm: </label>
       <select name="producttype_id" class="form-control form-select w-50">
-        <option value="">Loại sản phẩm</option>
+        <option value="">Chọn ()</option>
         @foreach ($producttypes as $producttype)
         <option value="{{ $producttype->id }}" {{ $producttype->id == request()->input('producttype_id') ? 'selected' : '' }}>{{ $producttype->name }}</option>
         @endforeach
@@ -23,17 +23,17 @@
     <div class="form-group mb-0 d-flex align-items-center justify-content-around col-lg-5">
       <label class="mb-0">Thương hiệu: </label>
       <select name="trademark_id" class="form-control form-select w-50">
-        <option value="">Thương hiệu</option>
+        <option value="">Chọn ()</option>
         @foreach ($trademarks as $trademark)
         <option value="{{ $trademark->id }}" {{ $trademark->id == request()->input('trademark_id') ? 'selected' : '' }}>{{ $trademark->name }}</option>
         @endforeach
       </select>
     </div>
-    <button type="submit" type="button" class="btn btn-dark col-lg-2">
+    <button type="submit" type="button" class="btn btn-dark col-lg-1">
       <i class="fa-regular fa-filter"></i>
     </button>
   </form>
-  <div class="text-lg-right mt-2">
+  <div class="text-lg-right col-lg-2">
     <a href="/admin/products/add" class="btn-sm btn btn-success text-decoration-none">
       <div class="p-1">
         <i class="fa-solid fa-plus"></i> Thêm sản phẩm
